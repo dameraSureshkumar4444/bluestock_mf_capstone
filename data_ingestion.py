631a3beedb49ca2b22d7054f47635f9b03190cdd
Damera Sureh Kumar <damerasuresh4444@gmail.com>
2026-06-02 17:09:23 +0530

    Changes to be committed:

--- data_ingestion.py
+++ data_ingestion.py
@@ -1,34 +1,5 @@
 from pathlib import Path
 import pandas as pd
 
-RAW_PATH = Path("data/raw")
-
-csv_files = list(RAW_PATH.glob("*.csv"))
-
-print(f"\nFound {len(csv_files)} CSV files\n")
-
-for file in csv_files:
-
-    print("=" * 100)
-    print("FILE:", file.name)
-
-    try:
-        df = pd.read_csv(file)
-
-        print("\nShape:")
-        print(df.shape)
-
-        print("\nDtypes:")
-        print(df.dtypes)
-
-        print("\nHead:")
-        print(df.head())
-
-        print("\nMissing Values:")
-        print(df.isnull().sum())
-
-        print("\nDuplicate Rows:")
-        print(df.duplicated().sum())
-
-    except Exception as e:
-        print("ERROR:", e)
+fund_master=pd.read_csv(r'01_fund_master.csv')
+fund_master.to_csv('data/raw')
